f8e45f4aa3574c309be55fec889d1bf9bafb141c
Pravanjan G <pravanjan.gochhayat@yahoo.com>
2024-04-12 22:44:23 +0530

    API updated
    1) retrieve subscriber details added.
    2) Initialized 2 API routes - blogs and subscribers

---
+++ src/app/api/subscribers/route.ts
@@ -0,0 +1,10 @@
+import {NextResponse} from "next/server";
+
+export async function GET(request: Request) {
+
+    try {
+        return NextResponse.json({ "message": "Hello World!"},  { status: 200 });
+    } catch (err) {
+        return NextResponse.json({ error: 'failed to fetch data' }, { status: 500 });
+    }
+}
--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -6,6 +6,9 @@ const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const options = {};
 const dbName = "foodblogdb";
 
+const subscriberCOll = "subscribers";
+const blogsCollection = "blogs";
+
 if(!uri){
     throw new Error("$MONGODB_URI is missing");
 }
@@ -13,21 +16,42 @@ if(!uri){
 const client = new MongoClient(uri, options);
 
 type Subscriber = {
+    id ?: string,
     name: string,
     mailid: string,
-    pwdstring: string,
+    password ?: string,
     creationdate: Date,
     modifieddate: Date
 }
 
-export async function addSubscriber(subscriber: Subscriber){
+type Blogs = {
+    blogname: string,
+    title: string,
+    bloggerId: string,
+    bloggerName: string,
+    content: string,
+}
+/**********************************************************************************************************
+ *       Functions to insert, update, delete, find operation on  subscriberCOll collection
+ * ********************************************************************************************************/
+
+/*
+* This function inserts document to the collection
+*
+* @function
+* @async
+* @param {Subscriber} Subscriber - Object containing details of the subscriber
+* @return {Promise<Subscriber>} A promise which resolves to Subscriber object containing new "id"
+*                               and rejects with error message in case of failure.
+ */
+export async function addSubscriber(subscriber: Subscriber): Promise<any> {
     client.connect()
         .then( async connectedClient => {
             const db = connectedClient.db(dbName);
-            const {acknowledged, insertedId} = await db.collection("subscriptions").insertOne(subscriber);
+            const {acknowledged, insertedId} = await db.collection(subscriberCOll).insertOne(subscriber);
             await client.close();
             if(acknowledged){
-                return Promise.resolve({status:"success", insertedId: insertedId , ...subscriber});
+                return Promise.resolve({status:"success", subscriber: {id: insertedId.toString() , ...subscriber} });
             }else{
                 return Promise.reject({status: "error", error: "Insert Error", logmessage: "Failed to insert document into database"});
             }
@@ -38,12 +62,21 @@ export async function addSubscriber(subscriber: Subscriber){
         });
 }
 
-export async function  deleteSubscriber(subscriberid:ObjectId){
+/*
+* This function deletes document on the collection
+*
+* @function
+* @async
+* @param {number}  subscriber unique id
+* @return {Promise<number>} A promise which resolves to number of document deleted
+*                               and rejects with error message in case of failure.
+ */
+export async function  deleteSubscriber(subscriberid:string): Promise<any> {
     client.connect()
     .then( async connectedClient => {
         const db = connectedClient.db(dbName);
         const query = { _id: new ObjectId(subscriberid) };
-        const {acknowledged, deletedCount} = await db.collection("subscriptions").deleteOne(query);
+        const {acknowledged, deletedCount} = await db.collection(subscriberCOll).deleteOne(query);
         if(acknowledged && deletedCount){
             return Promise.resolve({status:"success", deletedCount: deletedCount});
         }else{
@@ -54,12 +87,21 @@ export async function  deleteSubscriber(subscriberid:ObjectId){
     });
 }
 
-export async function modifySubscriber(subscriberid: string, subscriber: Subscriber){
+/*
+* This function updates document of the collection
+*
+* @function
+* @async
+* @param {Subscriber} Subscriber - Object containing details of the subscriber
+* @return {Promise<number>} A promise which resolves to number of document updated
+*                               and rejects with error message in case of failure.
+ */
+export async function modifySubscriber( subscriber: Subscriber): Promise<any> {
     client.connect()
         .then( async connectedClient => {
             const db = connectedClient.db(dbName);
-            const query = { _id: new ObjectId(subscriberid) };
-            const {acknowledged, modifiedCount} = await db.collection("subscriptions").updateOne(query, {$set: subscriber} );
+            const query = { _id: new ObjectId(subscriber.id) };
+            const {acknowledged, modifiedCount} = await db.collection(subscriberCOll).updateOne(query, {$set: subscriber} );
             if(acknowledged && modifiedCount){
                 return Promise.resolve({status:"success", modifiedCount: modifiedCount});
             }else{
@@ -69,3 +111,38 @@ export async function modifySubscriber(subscriberid: string, subscriber: Subscri
         return Promise.reject({status: "error", error: error, logmessage: "Failed to connect database"});
     });
 }
+
+/*
+* This function retrieves document from the collection
+*
+* @function
+* @async
+* @param {string} id - id of the subscriber
+* @param {string} passwd - optional password string
+* @return {Promise<Subscriber>} A promise which resolves to Subscriber object
+*                               and rejects with error message in case of failure.
+ */
+export async function getSubscriber( id: string, passwd ?: string): Promise<any> {
+    client.connect()
+        .then( async connectedClient => {
+            const db = connectedClient.db(dbName);
+            let query ;
+            if(passwd !== undefined && passwd !== null){
+                query = { _id: new ObjectId(id), password: passwd };
+            }else {
+                query = { _id: new ObjectId(id) };
+            }
+            const subscriber = await db.collection(subscriberCOll).findOne(query, {projection: {password: 0}});
+            if(subscriber){
+                return Promise.resolve({status:"success", subscriber: subscriber});
+            }else{
+                return Promise.reject({status: "error", error: "Updation Error", logmessage: "Couldn't found subscriber"});
+            }
+        }).catch(error => {
+        return Promise.reject({status: "error", error: error, logmessage: "Failed to connect database"});
+    });
+}
+
+/**********************************************************************************************************
+ *           Functions to insert, update, delete, find operation on  blogsCollection collection
+ * ********************************************************************************************************/
